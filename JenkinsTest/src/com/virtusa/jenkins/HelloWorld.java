f4ea511868a03b8a44593a68f4dd515abda6911e
manishsingh1998 <61631407+manishsingh1998@users.noreply.github.com>
2020-04-05 23:42:04 +0530

    Update HelloWorld.java

--- JenkinsTest/src/com/virtusa/jenkins/HelloWorld.java
+++ JenkinsTest/src/com/virtusa/jenkins/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("This is the first program to be pushed into jenkins");
 		System.out.println("Checking Changes");
-		System.out.println("Checking Changes)
+		System.out.println("Github Webhook");
 	}
 
 }
