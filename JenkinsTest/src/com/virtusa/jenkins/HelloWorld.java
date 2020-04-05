e6d7ffb3b2eb6e7b281a6653a77d8d35125f0fde
manishsingh1998 <manishsinghadhikari44@gmail.com>
2020-04-05 22:58:22 +0530

    commit 2

--- JenkinsTest/src/com/virtusa/jenkins/HelloWorld.java
+++ JenkinsTest/src/com/virtusa/jenkins/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World");
-
+		System.out.println("This is the first program to be pushed into jenkins");
 	}
 
 }
